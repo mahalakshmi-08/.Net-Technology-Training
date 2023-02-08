c62044cc611a8aa1ca734549e18e1226da59a8b3
mahalakshmi-08 <71280179+mahalakshmi-08@users.noreply.github.com>
2023-02-08 09:45:22 +0530

    Create c# ConditionalStatementsDemo.cs

---
+++ day7/c# ConditionalStatementsDemo.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace ConditionalStatementDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even Number");
+            }
+            else
+            {
+                Console.WriteLine("Odd Number");
+            }
+        }
+    }
+}
